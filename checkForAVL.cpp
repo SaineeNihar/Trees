04f0ca7927b3e5a9d78c1bdbcd01a267a0fe80c4
Nihar Sainee <54977872+SaineeNihar@users.noreply.github.com>
2020-07-23 23:47:10 +0530

    Add files via upload

---
+++ checkForAVL.cpp
@@ -0,0 +1,138 @@
+#include<bits/stdc++.h>
+using namespace std;
+class AVLNode
+{
+    public:
+    int data,height;
+    AVLNode *lc,*rc;
+    AVLNode(int d)
+    {
+        data=d;
+        lc=NULL;
+        rc=NULL;
+        height=1;
+    }
+};
+int AVLheight(AVLNode *root)
+{
+    if(!root)
+        return -1;
+    return root->height; 
+}
+AVLNode *signleRR(AVLNode *node)
+{
+    AVLNode *tmp=node->lc;
+    node->lc=tmp->rc;
+    tmp->rc=node;
+    node->height=max(AVLheight(node->lc),AVLheight(node->rc))+1;
+    tmp->height=max(AVLheight(tmp->lc),node->height)+1;
+    return tmp;
+}
+AVLNode *signleLR(AVLNode *node)
+{
+    AVLNode *tmp=node->rc;
+    node->rc=tmp->lc;
+    tmp->lc=node;
+    node                                                                                                                                                                                                                                                                                                                                                                                                                                                                   ->height=max(AVLheight(node->lc),AVLheight(node->rc))+1;
+    tmp->height=max(AVLheight(tmp->rc),node->height)+1;
+    return tmp;
+}
+int getBalance(AVLNode *node)
+{
+    if(!node)
+        return 0;
+    return AVLheight(node->lc)-AVLheight(node->rc);
+}
+AVLNode *insert(AVLNode *node, int x)
+{
+    if(!node)
+        return new AVLNode(x);
+    if(x<node->data)
+        node->lc=insert(node->lc,x);
+    else if(x>node->data)
+        node->rc=insert(node->rc,x);
+    else 
+        return node;
+    node->height=max(AVLheight(node->lc),AVLheight(node->rc))+1;
+    int bal=getBalance(node);
+    if(bal>1 and x<node->lc->data)
+        return signleRR(node);
+    if(bal<-1 and x>node->rc->data)
+        return signleLR(node);
+    if(bal>1 and x>node->lc->data)
+    {
+        node->lc=signleLR(node->lc);
+        return signleRR(node);
+    }
+    if(bal<-1 and x<node->rc->data)
+    {
+        node->rc=signleRR(node->rc);
+        return signleLR(node);
+    }
+    return node;
+}
+void NRinorder(AVLNode *root)
+{
+    if(!root)
+        return;
+    stack<AVLNode*> s;
+    while (1)
+    {
+        while (root)
+        {
+            s.push(root);
+            root=root->lc;
+        }
+        if(s.empty())
+            break;
+        cout<<s.top()->data<<"   ";
+        root=s.top();s.pop();
+        root=root->rc;
+    }
+    cout<<endl;
+}
+void preOrder(AVLNode *root)  
+{  
+    if(root != NULL)  
+    {  
+        cout << root->data << " ";  
+        preOrder(root->lc);  
+        preOrder(root->rc);  
+    }  
+}
+int checkForAVL(AVLNode *root)
+{
+    if(!root)
+        return 0;
+    int l,r;
+    l=checkForAVL(root->lc);
+    if(l==-1)
+        return l;
+    r=checkForAVL(root->rc);
+    if(r==-1)
+        return r;
+    if(abs(l-r)>1)
+        return -1;
+    return max(l,r)+1;
+}
+int main()
+{
+    AVLNode *root = NULL;  
+    /* Constructing tree given in  
+    the above figure */
+    cout<<"hey1";
+    root = insert(root, 10);  
+    root = insert(root, 20);  
+    root = insert(root, 30);  
+    root = insert(root, 40);  
+    root = insert(root, 50);  
+    root = insert(root, 25);
+    cout<<"hey2"; 
+    preOrder(root);
+    if(checkForAVL(root)==-1)
+        cout<<"Not an AVL Tree";
+    else
+    cout<<"AVL Tree";
+    cout<<endl;
+    return 0;
+}
